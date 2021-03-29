4e5abac6f62262bc6035fe0398b218580f5bc5c4
TouchedByDarkness <53915513+TouchedByDarkness@users.noreply.github.com>
2021-03-30 03:18:06 +0500

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Void Theme
+// @version      1.1
+// @description  none
+// @author       Darkness
+// @match        https://pixelplanet.fun/*
+// @match        https://fuckyouarkeros.fun/*
+// @updateURL    https://raw.githubusercontent.com/TouchedByDarkness/pixelplanet-void-style/main/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/TouchedByDarkness/pixelplanet-void-style/main/script.user.js
+// ==/UserScript==
+
+ssv.availableStyles.void = "https://raw.githubusercontent.com/TouchedByDarkness/pixelplanet-void-style/main/style.css";
